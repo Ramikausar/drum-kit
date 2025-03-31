3903f1867bc96194e80fbe430959abed6ca28c71
kausar <kausarrami212@gmail.com>
2025-03-31 22:06:58 +0530

    sound added as per alphabet

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 var NumOfDrumButton = document.querySelectorAll(".drum").length;
-for (var i =0; i< NumOfDrumButton; i++){
+for (var i =0; i< NumOfDrumButton; i++)
+{
 
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
     
-    function handleClick(){
-        var audio = new Audio("sound/tom-4.mp3");
-        audio.play();
-    } 
+    var buttonInnerHTML =this.innerHTML;
+
+
+        switch (buttonInnerHTML) 
+        {
+            case "w": 
+                var tom1 = new Audio("sound/tom-1.mp3");
+                tom1.play();
+            break;
+
+            case "a": 
+                var tom2 = new Audio("sound/tom-2.mp3");
+                tom2 .play();
+            break;
+
+            case "s": 
+                var tom3 = new Audio("sound/tom-3.mp3");
+                tom3.play();
+            break;
+
+            case "d": 
+                var tom4 = new Audio("sound/tom-4.mp3");
+                tom4 .play();
+            break;
+
+            case "j": 
+                var snare = new Audio("sound/snare.mp3");
+                snare.play();
+            break;
+
+            case "k": 
+                var cash = new Audio("sound/cash.mp3");
+                cash.play();
+            break;
+
+            case "l": 
+                var kick = new Audio("sound/kick.mp3");
+                kick.play();
+            break;
+
+            default: console.log(buttonInnerHTML);
+            
+
+        }
+    });
 
 }
+
   
